2a3419a0996e10adcb0a4a69017e0f33435e484c
Daniel Rodrigues <96748212+danniel-rodrigues@users.noreply.github.com>
2022-06-13 11:58:32 -0300

    Add classe para conversão de dolar -> real e vice-versa

--- script.js
+++ script.js
@@ -7,12 +7,35 @@ let timeFormat = (dateTime)  =>  {
     return joinDateTime;
 };
 
+
 let updateHTML = (data) => {
     dateTime = data.USDBRL.create_date;
-    document.getElementById('dolar-update').innerText = parseFloat(data.USDBRL.bid).toPrecision(3);
+    dolar_update = parseFloat(data.USDBRL.bid).toPrecision(3);
+    document.getElementById('dolar-update').innerText = dolar_update;
     document.getElementById('time-update').innerText = timeFormat(dateTime);
 
+    value_conversion(dolar_update);
+
 };
+
+
+// Convertendo dolar para real e vice-versa
+let value_conversion = (dolar_update) => {
+    let dolar = document.getElementById('dolar-value');
+    let real = document.getElementById('real-value');
+
+    dolar.value = 1.00.toFixed(2);
+    real.value = dolar_update;
+
+    if(dolar.addEventListener('keyup', () => {
+        real.value = (dolar_update * dolar.value).toFixed(2);
+    }));
+
+    if(real.addEventListener('keyup', () => {
+        dolar.value = (real.value / dolar_update).toFixed(2);
+    }));
+};
+
 
 // Obtém cotação do dólar por meio da AwesomeAPI
 let DollarQuote = async() => {
@@ -23,5 +46,6 @@ let DollarQuote = async() => {
     updateHTML(data);
 };
 
+
 // Atualiza função da cotação do dólar a cada 1 minuto
-setInterval(() => { DollarQuote(); }, 60000);
+setInterval(() => { DollarQuote();}, 60000);
