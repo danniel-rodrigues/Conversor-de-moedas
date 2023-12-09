235cb174c9ef48e9187ccd2f68a8c0c0387c8ae3
Daniel Rodrigues <danielrodrigues.dev@gmail.com>
2023-12-09 14:14:12 -0300

    Refeito as funções de captura, ordenação, remoção e preenchimento dos selects

--- script.js
+++ script.js
@@ -1,58 +1,86 @@
-async function getNameOfCurrencies() {
-    return fetch('https://economia.awesomeapi.com.br/json/available/uniq')
-        .then((data) => data.json())
-        .catch((err) => console.log(err))
+const API_URL = "https://economia.awesomeapi.com.br/json/available/uniq"
+
+const getNamesOfCurrencies = () => {
+    const options = {
+        method: "GET",
+        mode: "cors",
+        cache: "default"
+    }
+
+    fetch(API_URL, options)
+    .then(response => response.json())
+    .then(data => {
+        // Remover elementos específicos do array
+        const filteredCurrencies = removeCurrencies(data, [
+            "BRLT", "CHFRTS", "JPYRTS", "NGNI", "NGNPARALLEL", "SDR", "RUBTOD", "RUBTOM", "USDT", "XAU", "XAGG"
+        ])
+
+        const modifiedCurrencies = modifyCurrencyName(filteredCurrencies, "TMT", "Manat Turcomano")
+        
+        const sortedCurrencies = alphabeticSort(modifiedCurrencies)
+
+        populateSelects(sortedCurrencies)
+    })
+    .catch(error => console.log("Erro ao obter dados da API: ", error))
 }
 
-async function formatCurrencyNames() {
-    let currencyMap = new Map()
-    let data = await getNameOfCurrencies()
-    let formattedData = JSON.stringify(data).replace('{',"").replace('}',"")
-                    .replaceAll(",", "\n").replaceAll(":", ",").split("\n").sort()
 
-    formattedData.forEach(dataEl => {
-        let splittedData = dataEl.replaceAll('\"',"").split(",")
-        currencyMap.set(splittedData[0],splittedData[1])
+// Função para ordenar os nomes das moedas em ordem alfabética
+const alphabeticSort = (currencies) => {
+
+    // Convertendo o objeto em um array de pares chave-valor
+    let currencyArray = Object.entries(currencies)
+
+    // Ordenando o array com base nos nomes
+    currencyArray.sort((a,b) => a[1].localeCompare(b[1]))
+
+    // Convertendo o array de pares chave-valor para objeto novamente
+    return Object.fromEntries(currencyArray)
+}
+
+
+// Função para remover elementos do array com base em uma lista de códigos
+const removeCurrencies = (currencies, codesToRemove) => {
+    const filteredCurrencies = Object.fromEntries(
+        Object.entries(currencies).filter(([code,_]) => !codesToRemove.includes(code))
+    )
+    
+    return filteredCurrencies
+}
+
+
+const modifyCurrencyName = (currencies, codeToModify, newName) => {
+    // Modificando o nome da moeda no array
+    const modifiedArray = Object.entries(currencies).map(([code, name]) => {
+        if (code === codeToModify) {
+            return [code, newName];
+        }
+        return [code, name];
     });
 
-    const sortedAsc = new Map([...currencyMap].sort((a,b) => (a[1] > b[1] ? 1 : -1)))
+    // Convertendo o array de volta para um objeto
+    const modifiedObject = Object.fromEntries(modifiedArray);
 
-    sortedAsc.delete("USDT")
-    sortedAsc.delete("BRLT")
-    sortedAsc.delete("SDR")
-    sortedAsc.delete("CHFRTS")
-    sortedAsc.delete("JPYRTS")
-    sortedAsc.delete("NGNI")
-    sortedAsc.delete("NGNPARALLEL")
-    sortedAsc.delete("RUBTOD")
-    sortedAsc.delete("RUBTOM")
-    sortedAsc.delete("XRP")
-    sortedAsc.delete("XAU")
-    sortedAsc.delete("XAGG")
-
-    sortedAsc.set("TMT", "Manat turcomano")
-
-    return sortedAsc
-    
-    // let moeda = "Real Brasileiro"
-
-    // currencyMap.forEach((value, key) => {
-    //     if(value == moeda) console.log(key)
-        
-    // })
-}
+    return modifiedObject;
+};
 
 
-async function addCurrenciesToSelects() {
-    let selectTo = document.getElementById("select-to")
+// Função para popular os selects com os nomes das moedas
+const populateSelects = (sortedCurrencies) => {
     let selectFrom = document.getElementById("select-from")
-    
-    let teste = await formatCurrencyNames()
-    teste.forEach((value,_) => {
-        selectTo.innerHTML += `<option>${value}</option>`
-        selectFrom.innerHTML += `<option>${value}</option>`
-    })
+    let selectTo = document.getElementById("select-to")
 
+    for(const [code, name] of Object.entries(sortedCurrencies)) {
+        let optionFrom = document.createElement("option")
+        optionFrom.value = code
+        optionFrom.text = name
+        selectFrom.appendChild(optionFrom)
+        
+        let optionTo = document.createElement("option")
+        optionTo.value = code
+        optionTo.text = name
+        selectTo.appendChild(optionTo)
+    }
 }
 
-addCurrenciesToSelects()
+getNamesOfCurrencies()
